7078c9c1030e6e14467f8bbf68b1c7d9584d7961
Malte Hildebrandt <malte.hildebrandt@durstexpress.eu>
2021-03-21 21:02:45 +0100

    final

--- network/models.py
+++ network/models.py
@@ -3,4 +3,13 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    follows = models.ManyToManyField("User", related_name="followers")
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    text = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likers = models.ManyToManyField("User", related_name="posts_liked")
+
+
+
--- network/views.py
+++ network/views.py
@@ -1,14 +1,75 @@
+import json
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
-from .models import User
-
+from .models import User, Post
 
 def index(request):
-    return render(request, "network/index.html")
+    if request.method == "POST":
+        newPost = Post(user=request.user, text=request.POST["text"])
+        newPost.save()
+        return HttpResponseRedirect(reverse("index"))
+
+    posts = Post.objects.all()
+    posts = posts.order_by("-timestamp").all()
+
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "network/index.html", {   
+        "posts" : page_obj
+    })
+
+def network(request):
+    users = User.objects.all()
+    return render(request, "network/network.html", {
+        "users" : users
+    })
+
+@login_required
+def following(request):
+    follows = request.user.follows.all()
+    posts = Post.objects.filter(user__in=follows)
+    posts = posts.order_by("-timestamp").all()
+    
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, "network/following.html", {
+        "posts" : page_obj
+    })
+
+def profile(request, user_id):
+    userviewed=User.objects.get(id=user_id)
+    numfollowers = userviewed.followers.all().count()
+    numfollows = userviewed.follows.all().count()
+    posts = Post.objects.filter(user=userviewed)
+    posts = posts.order_by("-timestamp").all()
+
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    follows = False
+    if request.user.is_authenticated:
+        if userviewed in request.user.follows.all():
+            follows = True
+
+    return render(request, "network/profile.html", {
+        "numfollowers" : numfollowers,
+        "numfollows" : numfollows,
+        "posts" : page_obj,
+        "userviewed" : userviewed,
+        "follows": follows
+    })
 
 
 def login_view(request):
@@ -61,3 +122,91 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+# API views
+
+@csrf_exempt
+@login_required
+def like(request):
+
+    # liking a Post must be via POST
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+
+    # get the data
+    data = json.loads(request.body)
+    postid = data.get("postid")
+    like = data.get("like")
+
+    try:
+            post = Post.objects.get(id=postid)
+            if like:
+                post.likers.add(request.user)
+            else:
+                post.likers.remove(request.user)
+            likercount = post.likers.all().count()
+            return JsonResponse({
+                        "message": "liked successfully.",
+                        "likecount": likercount
+                        },  status=201)
+
+    except Post.DoesNotExist:
+        return JsonResponse({
+            "error": f"Post with id {postid} does not exist."
+        }, status=400)
+
+@csrf_exempt
+@login_required
+def edit(request):
+
+    # editing a Post must be via POST
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+
+    # get the data
+    data = json.loads(request.body)
+    postid = data.get("postid")
+    text = data.get("text")
+    try:
+        post = Post.objects.get(id=postid)
+        post.text = text
+        post.save()
+        return JsonResponse({
+                    "message": "updated successfully.",
+                    },  status=201)
+
+    except Post.DoesNotExist:
+        return JsonResponse({
+            "error": f"Post with id {postid} does not exist."
+        }, status=400)
+
+@csrf_exempt
+@login_required
+def follow(request):
+
+    # editing a Post must be via POST
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+
+    # get the data
+    data = json.loads(request.body)
+    userid = data.get("userid")
+    follow = data.get("follow")
+    try:
+        user = User.objects.get(id=userid)
+        if follow:
+            request.user.follows.add(user)
+        else:
+            request.user.follows.remove(user)
+        request.user.save()
+        return JsonResponse({
+                    "message": "updated successfully.",
+                    },  status=201)
+
+    except Post.DoesNotExist:
+        return JsonResponse({
+            "error": f"user could not be followed / unfollowed."
+        }, status=400)
+
+
+
