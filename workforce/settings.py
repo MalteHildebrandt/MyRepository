1d859d461e8d17c59206834b8194a95504db4c61
Malte Hildebrandt <malte.hildebrandt@durstexpress.eu>
2021-04-16 12:49:46 +0200

    postgres configured

--- workforce/settings.py
+++ workforce/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import django_heroku
 import gunicorn
 import psycopg2.extensions
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,15 +78,19 @@ WSGI_APPLICATION = 'workforce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -96,6 +101,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
